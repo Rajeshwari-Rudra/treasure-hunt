02b52b5a3d8db6a5c8a35e401b5b124f4cab6a7e
Rajeshwari_Rudra <rajrudra.raj@gmail.com>
2020-11-16 08:19:15 -0500

    add quest.js to models

--- models/quest.js
+++ models/quest.js
@@ -6,3 +6,64 @@
  * 
  */
 
+// module.exports = (db, DataTypes) => {
+//     db.define('Quest', {
+//       // sqlite creates a rowid attribute automatically
+//       name: {
+//         type: DataTypes.STRING(30),
+//         unique: true,
+//         required: true,
+//         allowNull: false,
+//         defaultValue: 'Quest',
+//         validate: {
+//           is: {
+//             args: /^[A-Za-z]+$/i, // matches a RegExp
+//             msg: 'Name is only letters, no spaces or punctuation.',
+//           },
+//           notNull: {
+//             args: true,
+//             msg: 'Name cannot be null.',
+//           },
+//           notEmpty: {
+//             args: true, // RegExp- only letters, no spaces
+//             msg: 'Name cannot be empty.',
+//           },
+//           max: {
+//             args: [30],
+//             msg: 'Name is limited to 32 characters.',
+//           },
+//           min: {
+//             args: [3],
+//             msg: 'Name must be at least 3 characters.',
+//           },
+//         },
+//       },
+    
+      
+//       age: {
+//               type: DataTypes.INTEGER,
+//               defaultValue: 1,
+//               notEmpty: true,
+//               validate: {
+//                 max: {
+//                   args: 100,
+//                   msg: 'Age must be 100 or less.',
+//                 },
+//                 min: {
+//                   args: 1,
+//                   msg: 'Age must be 1 or more.',
+//                 },
+//               },
+  
+//           },
+      
+      
+      
+      
+      
+//       isCartoon: {
+//         type: DataTypes.BOOLEAN,
+//         defaultValue: true,
+//       },
+//     });
+//   };
