a0a5d1ec60921699dace2e0d53919866c5d88053
Rajeshwari-Rudra <rajeshwarihyd.02@gmail.com>
2020-11-20 15:23:31 -0500

    add competition model

---
+++ models/competition.js
@@ -0,0 +1,78 @@
+/**
+ *  Competition model
+ *  Describes the characteristics of each attribute in a Competition resource.
+ *
+ * @author Rajeshwari Rudravaram <s538361@nwmissouri.edu>
+ *
+ * For more information about defining sequelize models, see
+ * https://sequelize.org/v5/manual/data-types.html
+ * https://sequelize.org/master/manual/validations-and-constraints.html
+ *
+ * For validators see: https://github.com/validatorjs/validator.js
+ *
+ */
+// Export a function that defines the model.
+// It automatically receives the Sequelize connection parameter.
+
+module.exports = (db, DataTypes) => {
+    db.define('Competition', {
+      // sqlite creates a rowid attribute automatically
+      name: {
+        type: DataTypes.STRING(30),
+        unique: true,
+        required: true,
+        allowNull: false,
+        defaultValue: 'Competition',
+        validate: {
+          is: {
+            args: /^[A-Za-z]+$/i, // matches a RegExp
+            msg: 'Name is only letters, no spaces or punctuation.',
+          },
+          notNull: {
+            args: true,
+            msg: 'Name cannot be null.',
+          },
+          notEmpty: {
+            args: true, // RegExp- only letters, no spaces
+            msg: 'Name cannot be empty.',
+          },
+          max: {
+            args: [30],
+            msg: 'Name is limited to 32 characters.',
+          },
+          min: {
+            args: [3],
+            msg: 'Name must be at least 3 characters.',
+          },
+        },
+      },
+    
+      
+      age: {
+              type: DataTypes.INTEGER,
+              defaultValue: 1,
+              notEmpty: true,
+              validate: {
+                max: {
+                  args: 100,
+                  msg: 'Age must be 100 or less.',
+                },
+                min: {
+                  args: 1,
+                  msg: 'Age must be 1 or more.',
+                },
+              },
+  
+          },
+      
+      
+      
+      
+      
+      isCartoon: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: true,
+      },
+    });
+  };
+  
