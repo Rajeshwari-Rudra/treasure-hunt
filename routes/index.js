d42b8c8d4e8bfe3b8416734c92ee52609bfc8695
Rajeshwari-Rudra <rajeshwarihyd.02@gmail.com>
2020-11-28 17:56:43 -0500

    modifyy index route

--- routes/index.js
+++ routes/index.js
@@ -9,6 +9,18 @@
  */
 
 const express = require('express');
+const LOG = require('../util/logger');
+const userRouter = require('./users');
+
+const clueRoutes = require('./clue.routes');
+const competitionRoutes = require('./competition.routes');
+const locationRoutes = require('./location.routes');
+const playerRoutes = require('./player.routes');
+const questRoutes = require('./quest.routes');
+const teamRoutes = require('./team.routes');
+
+
+LOG.info('routes/index.js: STARTING custom routes......');
 
 const router = express.Router();
 
@@ -25,5 +37,48 @@ router.get('/', (req, res) => {
 router.get('/index', (req, res) => {
   res.render('index.ejs', { title: appTitle, subTitle: appSubTitle });
 });
+
+
+router.use('/user', userRouter);
+
+// better route to t-e-a-m before we route to t-e-a
+// team routing moved before tea routing
+try {
+  router.use('/team', teamRoutes);
+} catch (err) {
+  LOG.error(`ERROR: ${err.message}`);
+}
+
+try {
+  router.use('/clue', clueRoutes);
+} catch (err) {
+  LOG.error(`ERROR: ${err.message}`);
+}
+try {
+  router.use('/competition', competitionRoutes);
+} catch (err) {
+  LOG.error(`ERROR: ${err.message}`);
+}
+
+try {
+  router.use('/location', locationRoutes);
+} catch (err) {
+  LOG.error(`ERROR: ${err.message}`);
+}
+
+try {
+  router.use('/quest', questRoutes);
+} catch (err) {
+  LOG.error(`ERROR: ${err.message}`);
+}
+
+try {
+  router.use('/player', playerRoutes);
+} catch (err) {
+  LOG.error(`ERROR: ${err.message}`);
+}
+
+
+LOG.info('routes/index.js: ENDING custom routes......');
 
 module.exports = router;
